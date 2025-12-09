da2b184e9557ad7334bb856946ae2022eda8705d
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 13:46:34 +0530

    Filter String

---
+++ Filter.py
@@ -0,0 +1,6 @@
+#Remove empty strings from the list of strings
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+
+# remove None from list1 and convert result into list
+res = list(filter(None, list1))
+print(res)
