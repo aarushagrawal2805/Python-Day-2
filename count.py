2b72e359df6ad96962de1fd79deddb2090dddf9d
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 13:22:10 +0530

    Count of elements

---
+++ count.py
@@ -0,0 +1,4 @@
+# Count and print how many times 'Football' appears in list
+Sports = ["Cricket", "Football", "Hockey", "Football", "Tennis", "Football", "Football"]
+
+print("Count of Football is:", Sports.count("Football"))
