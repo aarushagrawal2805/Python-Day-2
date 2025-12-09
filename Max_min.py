d19d2325753f2bc18f837c9183b65eb924188c15
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 13:17:55 +0530

    Max and min in list

---
+++ Max_min.py
@@ -0,0 +1,6 @@
+# Find Maximum and Minimum in List
+Data = [8,23,45,34,1]
+#Maximum Of List
+print("Maximum Element of List :",max(Data))
+#Minimum of list
+print("Minimum Element of List :",min(Data))
