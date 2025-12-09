3c3a9ab0f0140fc297becfee2a36698f8761e263
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 17:05:25 +0530

    Basic of Tuples

---
+++ Tbasic.py
@@ -0,0 +1,8 @@
+Item=(56,33,22,44,55,66)
+
+#Print tuple
+print("My tuple :",Item)
+#Print third element
+print("The third element of Tuple is :",Item[2])
+#Print Length of tuple
+print("Length of Tuple :",len(Item))
