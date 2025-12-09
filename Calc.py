18323d8a3fb5d1569d0257728758b9bb2db465a3
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 12:59:12 +0530

    Sum and avg

---
+++ Calc.py
@@ -0,0 +1,9 @@
+num = [10, 20, 30, 40, 50]
+
+#Calculate and print the sum 
+print("Sum :",sum(num)) # Using Sum Built-in Function
+
+#Calculate and print the Average of all numbers in a list.
+
+avg=sum(num)/len(num)
+print("Average :",avg)
