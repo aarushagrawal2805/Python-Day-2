04067466d6e1f84f75293519a27c6c4271650bd0
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 12:53:32 +0530

    List Manipulation

--- Manipulation.py
+++ Manipulation.py
@@ -3,20 +3,20 @@ print("List Before :",Item)
 
 #Change the second element of a list to 200 and print the updated list.
 Item[1] = 200
-print("List After :",Item)
+print("After changing second element :",Item)
 
 #Add 600 at the end of a list and print the new list
 Item.append(600)
-print("List After Append :",Item)
+print("List after appending 600 :",Item)
 
 #Insert 300 at the third position (index 2) of a list and print the result
 Item.insert(2,300)
-print("After insert :",Item)
+print("List after inserting 300 at index 2:",Item)
 
 #Remove 600 from the list and print the list
 Item.remove(600)
-print("After Remove By element :",Item)
+print("List after removing 600 (by value) :",Item)
 
 #Remove the element at index 0 from the list print the list
 Item.pop(0) #We can also use del function
-print("After Remove By Index :",Item)
+print("List after removing element at index 0 :",Item)
