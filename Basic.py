b9ef2fdd873a475379d9100d58669adfba108d38
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 12:36:04 +0530

    Basic List Operations

--- Basic.py
+++ Basic.py
@@ -1,2 +1,16 @@
+
 Item = [20,23,34,55]
 print(Item)
+
+#Print the third element
+print("Third Element In List",Item[3])
+
+#Print the number of elements in the list
+print("Length of List: ",len(Item))
+
+#Write a code to check is list empty.
+if len == 0:
+    print("List is empty")
+
+else:
+    print("List is not  empty")
