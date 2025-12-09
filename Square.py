0e3de4e475b1fa23fe1b854c9e04a4ff9ceb167c
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 13:13:24 +0530

    Sqaure of every element

---
+++ Square.py
@@ -0,0 +1,10 @@
+Item =[1,2,3,4,5,6]
+print("Initial List :",Item)
+
+#Given a list of numbers. write a program to turn every item of a list into its square
+
+Sqr=[]
+for i in Item:
+    Sqr.append(i*i)  #1*1,2*2 _ _ 6*6
+
+print("Square of every item in list",Sqr)
