3e24b403b346c4eebd31de973582a4cb8ae6fc5e
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-09 13:25:28 +0530

    Count and Sort

---
+++ count_sort.py
@@ -0,0 +1,9 @@
+# Count and print how many times 'Football' appears in list
+Sports = ["Cricket", "Football", "Hockey", "Football", "Tennis", "Football", "Football"]
+
+print("Count of Football is:", Sports.count("Football"))
+
+#Sort a list of string
+print("Before Sort :",Sports)
+Sports.sort()
+print("Sorted list :",Sports)
